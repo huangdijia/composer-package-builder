762706ae4a6f964d3e46c1189f0e6d2ee88b53d3
hdj <hdj@addcn.com>
2020-10-26 08:35:05 +0800

    fix errors

--- src/Plugin.php
+++ src/Plugin.php
@@ -18,6 +18,17 @@ class Plugin implements PluginInterface, Capable
      */
     public function activate(Composer $composer, IOInterface $io)
     {
+        //
+    }
+
+    public function deactivate(Composer $composer, IOInterface $io)
+    {
+        //
+    }
+
+    public function uninstall(Composer $composer, IOInterface $io)
+    {
+        //
     }
 
     public function getCapabilities()
